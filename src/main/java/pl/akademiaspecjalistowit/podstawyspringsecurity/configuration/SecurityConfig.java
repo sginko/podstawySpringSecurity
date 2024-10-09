f4e1bb975d63bc5c51595a9ca2482fdc0deeb156
sginko <142085017+sginko@users.noreply.github.com>
2024-10-09 17:08:05 +0200

    changes

--- src/main/java/pl/akademiaspecjalistowit/podstawyspringsecurity/configuration/SecurityConfig.java
+++ src/main/java/pl/akademiaspecjalistowit/podstawyspringsecurity/configuration/SecurityConfig.java
@@ -3,9 +3,12 @@ package pl.akademiaspecjalistowit.podstawyspringsecurity.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -40,6 +43,24 @@ public class SecurityConfig {
                 .httpBasic(withDefaults());
         return http.build();
     }
+
+//    @Bean
+//    UserDetailsManager users(DataSource dataSource) {
+////        UserDetails user = User.builder()
+////                .username("user")
+////                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+////                .roles("USER")
+////                .build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+//                .roles("USER", "ADMIN")
+//                .build();
+//        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+////        users.createUser(user);
+//        users.createUser(admin);
+//        return users;
+//    }
 
 //    @Bean
 //    public UserDetailsService userDetailsService(DataSource dataSource) {
